48ec52d55105ccf23341201d979efee044b05d2d
nebil <nebilgokdemir1@gmail.com>
2020-08-18 17:26:24 -0700

    set tableview

--- toDoTask/TableViewConteoller.swift
+++ toDoTask/TableViewConteoller.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewConteoller: UITableViewController {
 
+    var myArray = ["nebil","gokdemir","izzet","altun","mehmet","karatas"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        
+    
     }
+    
+
+    
+    
 
 
 }
---
+++ toDoTask/TableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TableViewController.swift
+//  toDoTask
+//
+//  Created by nebil on 8/18/20.
+//  Copyright © 2020 nebilgokdemir. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var myArray = ["nebil","mehmet","izzet","kazim","suleyman"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return myArray.count
+    }
+
+  
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = myArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("nebil")
+    }
+    
+}
+
+
